3902809250410ef38ecfe783fb2fe78fcda31119
Marcio Shikasho <mksdeveloper@hotmail.com>
2025-05-11 22:00:59 -0300

    ajustes

--- servico-pagamento/src/pagamento/pagamento.module.ts
+++ servico-pagamento/src/pagamento/pagamento.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { PagamentoController } from './pagamento.controller';
 import { PagamentoService } from './pagamento.service';
+import { PrismaModule } from '../prisma/prisma.module';
 
 @Module({
+  imports: [PrismaModule],
   controllers: [PagamentoController],
   providers: [PagamentoService]
 })
